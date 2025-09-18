b888f638e2dbcd64138e5c4373d3f0c2921488c1
I578504 <aryan.singh01@sap.com>
2025-09-18 14:01:02 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // Root path for BTP deployment
+  base: "/SIT-BLR-Vibeathon", // Root path for BTP deployment
   server: {
     host: "::",
     port: 8080,
